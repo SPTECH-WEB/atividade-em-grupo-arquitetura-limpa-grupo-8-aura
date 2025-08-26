f2a02a09404501e3c74ad3d1f648b57c11fcf3b5
LuizFelipeSptech <luiz.mendonca@sptech.school>
2025-08-26 19:35:17 -0300

    adding spring initializer files

---
+++ src/main/java/com/aura/risco_credito_api/RiscoCreditoApiApplication.java
@@ -0,0 +1,13 @@
+package com.aura.risco_credito_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiscoCreditoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiscoCreditoApiApplication.class, args);
+	}
+
+}
